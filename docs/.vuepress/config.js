90a6c589ee2bc29fb24e90e419a4c7153f3f4b0e
msqx <305562638@qq.com>
2019-10-13 20:39:50 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  title: '陌上青夏的开发书',
+  description: '陌上花开，可缓缓归矣',
+  themeConfig: {
+    repo: 'https://github.com/hulincloudmo',
+    serviceWorker: {
+      updatePopup: true
+    },
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '编程手记', link: '/guide/' },
+      { text: '心情笔录', link: '/mood/' },
+      { text: '博客链接', link: 'http://mookaihang.design' },
+
+    ],
+    sidebar: [
+      {
+        title: "Typescript",
+        collapsable: false,
+        children: [
+          ['guide/','初见Typescript'],
+          'guide/test'
+        ]
+      },
+      {
+        title: "微信小程序相关",
+        collapsable: false,
+        children: [
+          ['miniprogram/','微信小程序'],
+          'miniprogram/scroll-view'
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/miniprogram/README.md
@@ -0,0 +1,3 @@
+# 微信小程序开发过程中遇到的技术问题
+
+## 1. [为什么我的scroll-view的高度会如此之高？](/miniprogram/scroll-view.html)
